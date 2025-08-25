70a7f7cdd46ee5a88601e17f21f4dafe69cf5810
wobbat <mail@wobbat.com>
2025-08-25 19:39:37 +0200

    Stop tracking env.fish and env.sh, add to .gitignore

--- env.fish
+++
@@ -1,4 +0,0 @@
-# This file is managed by Owl package manager
-# Manual changes may be overwritten
-
-set -x XDG_SESSION_TYPE 'x11'
--- env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This file is managed by Owl package manager
-# Manual changes may be overwritten
-
-export XDG_SESSION_TYPE='x11'
