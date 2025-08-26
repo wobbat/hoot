4c752e4ea9aeba31b96b802de4c4cb62931e678e
wobbat <mail@wobbat.com>
2025-08-26 10:08:49 +0200

    changes

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -24,6 +24,7 @@ if status is-interactive
         fish_add_path $HOME/.cargo/bin
         fish_add_path $HOME/go/bin
         fish_add_path $HOME/.local/npm-global/bin
+        fish_add_path $HOME/.local/bin
         fish_add_path $HOME/.bun/bin
         [ -f ~/.owl/env.fish ] && source ~/.owl/env.fish
     case Darwin
